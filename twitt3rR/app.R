c33a5135c6c21fe374af5a4127656f7e5fc28d9d
JohnMikkelsonUST <mikk9317@stthomas.edu>
2019-05-11 17:14:13 +0000

    5/11 Update

--- twitt3rR/app.R
+++ twitt3rR/app.R
@@ -1,10 +1,53 @@
 library(shiny)
+library(ggplot2)
+library(tidyverse)
+library(lubridate)
+library(twitteR)
+library(tidytext)
+library(tidyr)
+library(purrr)
+library(scales)
+trump_tweets <- load(url("http://varianceexplained.org/files/trump_tweets_df.rda"))
 
 ui <- fluidPage(
+  titlePanel("Twitt3rR"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      textInput("Text", h3("Twitter Handle"), 
+                value = "Enter Twitter Handle...")
+    ),
+    mainPanel(
+      plotOutput("mobile")
+    )
+  )
   
 )
 
 server <- function(input, output) {
+  tweets <- trump_tweets_df %>%
+    select(id, statusSource, text, created) %>%
+    extract(statusSource, "source", "Twitter for (.*?)<") %>%
+    filter(source %in% c("iPhone", "Android"))
+  
+  output$mobile = renderPlot({
+    tweets %>%
+      count(source, hour = hour(with_tz(created, "EST"))) %>%
+      mutate(percent = n / sum(n)) %>%
+      ggplot(aes(hour, percent, color = source)) +
+      geom_line() +
+      scale_y_continuous(labels = percent_format()) +
+      labs(x = "Hour of day (EST)",
+           y = "% of tweets",
+           color = "")
+    
+    
+    ## ggplot here
+  })
+  
+  
+  
+  
   
 }
 
